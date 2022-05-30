6759d7b971e16c76ecdc17fbb5c30330e7af4c79
KROKODILMALBORO <krokodilmalboro@gmail.com>
2022-05-31 01:26:55 +0700

    Add basic project skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
+import Header from './components/Header'
+import TodoList from './components/TodoList'
+
 const App = () => {
     return (
         <div>
+            <Header />
+            <TodoList />
         </div>
     )
 }
